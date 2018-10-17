ff5ac1e960ca44a959683bbd8ea81d4400c776f1
fionwan <fion.wan8@gmail.com>
2018-10-17 17:13:38 -0600

    deploying to heroku round 2

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-
-func main() {
-	InitDB() 
-	router := NewRouter()
-
-	fmt.Println("server listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":8080", nil
+	}
+	return ":" + port, nil
 }
 
+func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	InitDB()
+	router := NewRouter()
+
+	fmt.Println("server listening on port", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
+}
